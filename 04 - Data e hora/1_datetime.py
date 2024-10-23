4dab9260d6d4dcf9cd1c5e4d82254871b9911c30
ronaldoramos85 <ronaldo.oliveira.ramos@gmail.com>
2024-10-22 21:34:58 -0300

    Update 1_datetime.py

--- 04 - Data e hora/1_datetime.py
+++ 04 - Data e hora/1_datetime.py
@@ -1,4 +1,6 @@
-from datetime import date, datetime, time
+from datetime import date, datetime, time, timezone
+
+'''
 
 data = date(2023, 7, 10)
 print(data)
@@ -11,3 +13,18 @@ print(datetime.today())
 
 hora = time(10, 20, 0)
 print(hora)
+
+
+
+data_teste = date(2020,10,20)
+print(f"data_teste : {data_teste}")
+delta = date.today() - data_teste
+print(f"Decorreram {delta} desde {data_teste} até hoje ({date.today()})")'''
+
+print(f"Valor de today: {datetime.today()}")
+
+print(f"Valor de datetime.now: {datetime.now()}")
+
+print(f"Valor de datetime.utcnow: {datetime.utcnow()}")
+
+print(f"Valor de datetime.now(timezone.utc): {datetime.now(timezone.utc)}")
